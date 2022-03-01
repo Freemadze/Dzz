6b341b5e954db5b40e11f48636acc3dd0c055821
Freemadze <97775954+Freemadze@users.noreply.github.com>
2022-03-01 23:05:52 +0500

    +

---
+++ src/Catss.java
@@ -0,0 +1,27 @@
+public class Catss {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat11;
+        Cat cat12;
+        Cat cat13;
+        Cat cat14;
+        Cat cat15;
+        Cat cat16;
+        Cat cat17;
+        Cat cat18;
+        Cat cat19;
+        Cat cat20;
+
+    }
+    private static class Cat{
+
+    }
+}
+
---
+++ src/catfishdog.java
@@ -0,0 +1,26 @@
+public class catfishdog {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Father Father = new Father();
+
+        fish.owner = Father;
+        cat.owner = Father;
+        dog.owner = Father;
+
+    }
+    private static class Fish{
+        public Father owner;
+
+    }
+    private static class Father{
+
+
+    }
+    private static class Cat {
+        public Father owner;
+    }
+    private static class Dog{
+        public Father owner;
+}
